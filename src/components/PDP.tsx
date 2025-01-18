6dc76011e93948131adc8b0c4b34cfded443de94
amaity-digi <amaity@digisprint.com>
2025-01-18 16:51:54 +0530

    feat(navigation): implemented stack and buttom navigation

---
+++ src/components/PDP.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const PDP = () => {
+  return (
+    <View>
+      <Text>PDP Page</Text>
+    </View>
+  );
+};
+
+export default PDP;
