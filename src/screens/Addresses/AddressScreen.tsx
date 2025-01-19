db38392c4b94bcd8d2df9a1dbb5c4fa874cb0b44
amaity-digi <amaity@digisprint.com>
2025-01-20 00:37:16 +0530

    feat(checkout): implemented cart item in checkout page

---
+++ src/screens/Addresses/AddressScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AddressesScreen = () => {
+  return (
+    <View>
+      <Text>{'AddressesScreen Page'}</Text>
+    </View>
+  );
+};
+
+export default AddressesScreen;
